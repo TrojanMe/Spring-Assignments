3548051c85c7b3ae940767280b39bda199e89a27
trojanMe <mitrasree@cokreates.com>
2019-03-11 15:47:46 +0600

    Added Assignment 01

---
+++ spring-demo-one/src/com/luv2code/springdemo/Archery.java
@@ -0,0 +1,11 @@
+package com.luv2code.springdemo;
+
+public class Archery implements Coach
+{
+	@Override
+	public String getDailyWorkout()
+	{
+		return "Get 30 minutes of archery practice";
+	}
+
+}
---
+++ spring-demo-one/src/com/luv2code/springdemo/BaseballCoach.java
@@ -0,0 +1,11 @@
+package com.luv2code.springdemo;
+
+public class BaseballCoach implements Coach
+{
+	@Override
+	public String getDailyWorkout()
+	{
+		return "Get 30 minutes of batting practice";
+	}
+
+}
